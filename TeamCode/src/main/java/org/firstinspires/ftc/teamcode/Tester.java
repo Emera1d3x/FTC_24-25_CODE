bdf2c0fc60ba576249cea3b96b7c9b623e08e7a8
Sehan Munir <162743866+Emera1d3x@users.noreply.github.com>
2024-12-11 21:32:16 -0500

    Update Tester.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tester.java
@@ -3,8 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "CyberLyons")
 public class Tester extends OpMode {
@@ -14,9 +12,6 @@ public class Tester extends OpMode {
     DcMotor motorLBWheel;
     DcMotor motorRFWheel;
     DcMotor motorRBWheel;
-    Servo clawServoRight;
-    Servo clawServoLeft;
-    public
     // JOYSTICK VARS
     float leftStickX;
     float leftStickY;
@@ -25,13 +20,11 @@ public class Tester extends OpMode {
 
 
     public void initializeStuff(){
-        motorArm = hardwareMap.get(DcMotor.class, "motorArm");
-        motorLFWheel = hardwareMap.get(DcMotor.class, "motorLF");
+        //motorArm = hardwareMap.get(DcMotor.class, "motorArm");
         motorLBWheel = hardwareMap.get(DcMotor.class, "motorLB");
-        motorRFWheel = hardwareMap.get(DcMotor.class, "motorRF");
         motorRBWheel = hardwareMap.get(DcMotor.class, "motorRB");
-        clawServoRight = hardwareMap.get(Servo.class, "clawRight");
-        clawServoLeft = hardwareMap.get(Servo.class, "clawLeft");
+        //clawServoRight = hardwareMap.get(Servo.class, "clawRight");
+        //clawServoLeft = hardwareMap.get(Servo.class, "clawLeft");
     }
 
     @Override
@@ -44,29 +37,31 @@ public class Tester extends OpMode {
     @Override
     public void loop() {
         // DEFAULTS
-        motorArm.setPower(0);
+        //motorArm.setPower(0);
+        motorLBWheel.setPower(0.0);
+        motorRBWheel.setPower(0.0);
         // STICK VALUES
-        float leftStickX = gamepad1.left_stick_x;
-        float leftStickY = gamepad1.left_stick_y;
-        float rightStickX = gamepad1.right_stick_x;
-        float rightStickY = gamepad1.right_stick_y ;
+        leftStickX = gamepad1.left_stick_x; // Right (+), Left (-)
+        leftStickY = -gamepad1.left_stick_y; // Up (+), Down (-)
         // MOVEMENT
-        if (leftStickX>0){
-            motorLFWheel.setPower(leftStickY);
-            motorLBWheel.setPower(leftStickY);
-            motorRFWheel.setPower(leftStickY+(leftStickX));
-            motorRBWheel.setPower(leftStickY+(leftStickX));
-        } else {
-            motorLFWheel.setPower(leftStickY-(leftStickX));
-            motorLBWheel.setPower(leftStickY-(leftStickX));
-            motorRFWheel.setPower(leftStickY);
+        telemetry.addData("System Info", String.valueOf(leftStickX)+" "+String.valueOf(leftStickY));
+        if (leftStickX<=0){
+            motorRBWheel.setPower(leftStickY+Math.abs(leftStickX));
+            motorLBWheel.setPower(-leftStickY);
+        } else if (leftStickX>0){
             motorRBWheel.setPower(leftStickY);
+            motorLBWheel.setPower(-leftStickY-leftStickX);
         }
+        /*if (leftStickX>0){
+            motorLBWheel.setPower(leftStickY);
+            motorRBWheel.setPower((-leftStickY)+Math.abs(leftStickX));
+        } else {
+            motorLBWheel.setPower(leftStickY+Math.abs(leftStickX));
+            motorRBWheel.setPower(-leftStickY);
+        }*/
         // ARM CONTROL
-        if (gamepad1.right_bumper){motorArm.setPower(1);}
-        if (gamepad1.left_bumper){motorArm.setPower(-1);}
+        //if (gamepad1.right_bumper){motorArm.setPower(1);}
+        //if (gamepad1.left_bumper){motorArm.setPower(-1);}
         // CLAW
-        if (gamepad1.a){clawServoRight.setPosition(0);clawServoLeft.setPosition(0);}
-        if (gamepad1.b){clawServoRight.setPosition(0.3);clawServoLeft.setPosition(-0.3);}
     }
 }
