88a5dfb04fac5a638f15591fd71a70140601743f
Sehan Munir <162743866+Emera1d3x@users.noreply.github.com>
2024-12-20 09:17:07 -0500

    Update Tester.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tester.java
@@ -2,29 +2,33 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "CyberLyons")
 public class Tester extends OpMode {
     // MATERIALS
     DcMotor motorArm;
-    DcMotor motorLFWheel;
     DcMotor motorLBWheel;
-    DcMotor motorRFWheel;
     DcMotor motorRBWheel;
+    CRServo clawServoLeft;
+    CRServo clawServoRight;
     // JOYSTICK VARS
     float leftStickX;
     float leftStickY;
-    float rightStickX;
-    float rightStickY;
-
+    // REVERSE CONTROL VARS
+    int reverseControl = 1;
+    boolean reverseControlCanToggle = true;
+    ElapsedTime toggleTimer = new ElapsedTime();
 
     public void initializeStuff(){
-        //motorArm = hardwareMap.get(DcMotor.class, "motorArm");
-        motorLBWheel = hardwareMap.get(DcMotor.class, "motorLB");
-        motorRBWheel = hardwareMap.get(DcMotor.class, "motorRB");
-        //clawServoRight = hardwareMap.get(Servo.class, "clawRight");
-        //clawServoLeft = hardwareMap.get(Servo.class, "clawLeft");
+        motorArm = hardwareMap.get(DcMotor.class, "motorArm"); // Pin #0
+        motorLBWheel = hardwareMap.get(DcMotor.class, "motorLB"); // Pin #?
+        motorRBWheel = hardwareMap.get(DcMotor.class, "motorRB"); // Pin #?
+        clawServoRight = hardwareMap.get(CRServo.class, "servoRight"); // Pin #1
+        clawServoLeft = hardwareMap.get(CRServo.class, "servoLeft"); // Pin #0
+        toggleTimer.reset();
     }
 
     @Override
@@ -33,35 +37,39 @@ public class Tester extends OpMode {
         telemetry.addData("Launch Test:", "Successful");
         telemetry.update();
     }
-
     @Override
     public void loop() {
         // DEFAULTS
-        //motorArm.setPower(0);
+        motorArm.setPower(0);
         motorLBWheel.setPower(0.0);
         motorRBWheel.setPower(0.0);
+        clawServoLeft.setPower(0.0);
+        clawServoRight.setPower(0.0);
         // STICK VALUES
         leftStickX = gamepad1.left_stick_x; // Right (+), Left (-)
-        leftStickY = -gamepad1.left_stick_y; // Up (+), Down (-)
-        // MOVEMENT
+        leftStickY = -gamepad1.left_stick_y*reverseControl; // Up (+), Down (-)
+        // MOVEMENT CONTROL
         telemetry.addData("System Info", String.valueOf(leftStickX)+" "+String.valueOf(leftStickY));
+        telemetry.addData("Reloader:", String.valueOf(toggleTimer.seconds()));
+        telemetry.addData("Can Press:", String.valueOf(reverseControlCanToggle));
         if (leftStickX<=0){
             motorRBWheel.setPower(leftStickY+Math.abs(leftStickX));
             motorLBWheel.setPower(-leftStickY);
+            if (toggleTimer.seconds() > 5) {reverseControlCanToggle = true;}
+            if (gamepad1.y && reverseControlCanToggle){reverseControl*=-1;reverseControlCanToggle=false;toggleTimer.reset();}
         } else if (leftStickX>0){
             motorRBWheel.setPower(leftStickY);
             motorLBWheel.setPower(-leftStickY-leftStickX);
+            if (toggleTimer.seconds() > 5) {reverseControlCanToggle = true;}
+            if (gamepad1.y && reverseControlCanToggle){reverseControl*=-1;reverseControlCanToggle=false;toggleTimer.reset();}
         }
-        /*if (leftStickX>0){
-            motorLBWheel.setPower(leftStickY);
-            motorRBWheel.setPower((-leftStickY)+Math.abs(leftStickX));
-        } else {
-            motorLBWheel.setPower(leftStickY+Math.abs(leftStickX));
-            motorRBWheel.setPower(-leftStickY);
-        }*/
         // ARM CONTROL
-        //if (gamepad1.right_bumper){motorArm.setPower(1);}
-        //if (gamepad1.left_bumper){motorArm.setPower(-1);}
-        // CLAW
+        if (gamepad1.right_bumper){motorArm.setPower(1);}
+        if (gamepad1.left_bumper){motorArm.setPower(-1);}
+        if (gamepad1.a){clawServoLeft.setPower(1);clawServoRight.setPower(-1);}
+        if (gamepad1.b){clawServoLeft.setPower(-1);clawServoRight.setPower(1);}
+        // CLAW CONTROL
+        if (gamepad1.a){clawServoLeft.setPower(1);clawServoRight.setPower(-1);}
+        if (gamepad1.b){clawServoLeft.setPower(-1);clawServoRight.setPower(1);}
     }
 }
